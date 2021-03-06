1ef6b61b505bb2aa09bd2763af369baac85d1e82
Rafath Bin Zafar Auvee <61088535+Rafath-Bin-Zafar-Auvee@users.noreply.github.com>
2021-03-06 11:33:25 +0600

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,34 @@
+mylists = ["banana", "cherry", "apple"]
+# print(mylists)
+
+# mylists2 = [5, True, "apple", "apple"]
+# print(mylists2)
+
+# item = mylists[0]
+# print(item)
+
+# for i in mylists:
+#     print(i)
+
+# if "banana" in mylists:
+#     print("Yes")
+# else:
+#     print("No")
+
+# print(len(mylists))
+
+# mylists.append("Lemon")
+# print(mylists)
+
+# mylists.insert(1, "blueberry")
+# print(mylists)
+
+# item = mylists.pop()
+# print(item)
+# print(mylists)
+
+# item = mylists.remove("cherry")
+# print(mylists)
+
+item = mylists.clear()
+print(mylists)
