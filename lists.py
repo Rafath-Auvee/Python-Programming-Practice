ce1847e11331ead7f6b98a3a1bf92e5d3417eced
Rafath Bin Zafar Auvee <61088535+Rafath-Bin-Zafar-Auvee@users.noreply.github.com>
2021-03-06 14:02:07 +0600

    Add files via upload

--- lists.py
+++ lists.py
@@ -1,73 +1,99 @@
-mylists = ["banana", "cherry", "apple"]
-print(mylists)
+# mylists = ["banana", "cherry", "apple"]
+# print(mylists)
 
-item = mylists.sort()
-print(mylists)
-mylists.sort()
+# item = mylists.sort()
+# print(mylists)
+# mylists.sort()
 
-new_list = sorted(mylists)
+# new_list = sorted(mylists)
 
-print(mylists)
+# print(mylists)
 
-item = mylists.reverse()
-print(mylists)
+# item = mylists.reverse()
+# print(mylists)
 
-mylists2 = [5, True, "apple", "apple"]
-print(mylists2)
+# mylists2 = [5, True, "apple", "apple"]
+# print(mylists2)
 
-item = mylists[0]
-print(item)
+# item = mylists[0]
+# print(item)
 
-for i in mylists:
-    print(i)
+# for i in mylists:
+#     print(i)
 
-if "banana" in mylists:
-    print("Yes")
-else:
-    print("No")
+# if "banana" in mylists:
+#     print("Yes")
+# else:
+#     print("No")
 
-print(len(mylists))
+# print(len(mylists))
 
-mylists.append("Lemon")
-print(mylists)
+# mylists.append("Lemon")
+# print(mylists)
 
-mylists.insert(1, "blueberry")
-print(mylists)
+# mylists.insert(1, "blueberry")
+# print(mylists)
 
-item = mylists.pop()
-print(item)
-print(mylists)
+# item = mylists.pop()
+# print(item)
+# print(mylists)
 
-item = mylists.remove("cherry")
-print(mylists)
+# item = mylists.remove("cherry")
+# print(mylists)
 
-item = mylists.clear()
-print(mylists)
-
-
-ch = [0] * 5
-print(ch)
-
-myl = [1, 2, 3, 4, 5]
-
-new = myl + ch
-print(new)
+# item = mylists.clear()
+# print(mylists)
 
 
-mylist = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-a = mylist[1:5]
-print(a)
+# ch = [0] * 5
+# print(ch)
+
+# myl = [1, 2, 3, 4, 5]
+
+# new = myl + ch
+# print(new)
 
 
-mylist = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-a = mylist[:-5]
-print(a)
-
-mylist = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-a = mylist[::2]
-print(a)
+# mylist = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# a = mylist[1:5]
+# print(a)
 
 
-mylist = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-a = mylist[::-1]
-print(a)
+# mylist = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# a = mylist[:-5]
+# print(a)
+
+# mylist = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# a = mylist[::2]
+# print(a)
+
+
+# mylist = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# a = mylist[::-1]
+# print(a)
+
+
+# my1 = ["banana", "cherry", "apple"]
+
+# my2 = my1
+
+# my2.append("lemon")
+# print(my1)
+# print(my2)
+
+
+# my1 = ["banana", "cherry", "apple"]
+
+# my2 = my1.copy()
+
+# my2.append("lemon")
+# print(my1)
+# print(my2)
+
+
+mylist = [1, 2, 3, 4, 5, 6]
+
+sqaure = [i*i for i in mylist]
+
+print(mylist)
+print(sqaure)
