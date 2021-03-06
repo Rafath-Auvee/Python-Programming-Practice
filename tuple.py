a02453915e620f73d7eed0be7bd101aca53d0fc1
Rafath Bin Zafar Auvee <61088535+Rafath-Bin-Zafar-Auvee@users.noreply.github.com>
2021-03-06 14:23:58 +0600

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,35 @@
+# mytuple = ("Max", 28, Boston)
+# print(mytuple)
+
+# mytuple = "Max", 28, "Boston"
+# print(mytuple)
+
+
+# item = mytuple[1]
+# print(item)
+
+
+# for i in mytuple:
+#     print(i)
+
+
+# if "Max" in mytuple:
+#     print("Yes")
+
+# else:
+#     print("No")
+
+my_tuple = ['a', 'p', 'p', 'l', 'e']
+# print(len(my_tuple))
+
+
+# print(my_tuple.count('e'))
+
+# print(my_tuple.index('p'))
+
+
+list1 = list(my_tuple)
+print(list1)
+
+tuple2 = tuple(list1)
+print(tuple2)
