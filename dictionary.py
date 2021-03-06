c9d4d451b80868084ff4a735745468a356527109
Rafath Bin Zafar Auvee <61088535+Rafath-Bin-Zafar-Auvee@users.noreply.github.com>
2021-03-07 04:26:05 +0600

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,5 @@
+dict1 = {"name": "max", "age": 14, "city": "chittagong"}
+print(dict1)
+
+dict2 = dict(name="Auvee", age=21, city="chittagong")
+print(dict2)
--- tuple.py
+++ tuple.py
@@ -1,31 +1,31 @@
-# mytuple = ("Max", 28, Boston)
-# print(mytuple)
+mytuple = ("Max", 28, Boston)
+print(mytuple)
 
-# mytuple = "Max", 28, "Boston"
-# print(mytuple)
-
-
-# item = mytuple[1]
-# print(item)
+mytuple = "Max", 28, "Boston"
+print(mytuple)
 
 
-# for i in mytuple:
-#     print(i)
+item = mytuple[1]
+print(item)
 
 
-# if "Max" in mytuple:
-#     print("Yes")
+for i in mytuple:
+    print(i)
 
-# else:
-#     print("No")
+
+if "Max" in mytuple:
+    print("Yes")
+
+else:
+    print("No")
 
 my_tuple = ['a', 'p', 'p', 'l', 'e']
-# print(len(my_tuple))
+print(len(my_tuple))
 
 
-# print(my_tuple.count('e'))
+print(my_tuple.count('e'))
 
-# print(my_tuple.index('p'))
+print(my_tuple.index('p'))
 
 
 list1 = list(my_tuple)
@@ -33,3 +33,28 @@ print(list1)
 
 tuple2 = tuple(list1)
 print(tuple2)
+tuple = "Rafath", 18, "ctg"
+
+name, age, city = tuple
+
+print(name)
+print(age)
+print(city)
+
+
+import sys
+
+lis1 = [0, 1, 2, 3, "hello", True]S
+tup1e = (0, 1, 2, 3, "hello", True)
+
+print(sys.getsizeof(lis1), "bytes")
+
+print(sys.getsizeof(tup1e), "bytes")
+
+
+import timeit
+
+print(timeit.timeit(stmt="[0,1,2,3,4,5]", number=1000000))
+
+
+print(timeit.timeit(stmt="(0,1,2,3,4,5)", number=1000000))
