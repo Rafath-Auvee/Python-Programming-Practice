87cae90df32710d96c695574bda7d9e7dc9512bc
Rafath Bin Zafar Auvee <61088535+Rafath-Bin-Zafar-Auvee@users.noreply.github.com>
2021-03-07 06:55:57 +0600

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,43 @@
+# myset = {1, 2, 3, 4, 1, 2, 3}
+
+# str = set("Rafath")
+# print(myset)
+
+# print(str)
+
+# odds = {1, 3, 5, 7, 9}
+# evens = {0, 2, 4, 6, 8, 10}
+# primes = {2, 3, 5, 7}
+
+# u = odds.union(evens)
+# print(u)
+
+# i = evens.intersection(primes)
+# print(i)
+
+
+# setA = {1, 2, 3}
+# setB = {1, 2, 3, 10, 11, 12}
+
+# setA.update(setB)
+# print(setA)
+# diff = setB.symmetric_difference(setA)
+
+# setA.intersection_update(setB)
+# print(setA)
+
+# setA.symmetric_difference_update(setB)
+
+# print(setA)
+
+# print(setA.issubset(setB))
+
+# print(diff)
+
+# diff = setA.difference(setB)
+# print(diff)
+
+
+a = frozenset([1, 2, 3, 4])
+
+print(a)
